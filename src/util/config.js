5e88a560922da2e0a6dd6915a5f0a46c2539d460
Lochlan Bennett-Odlum <lodlum@me.com>
2023-12-09 00:09:39 +0000

    Update config.js

--- src/util/config.js
+++ src/util/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://skylight-photography.herokuapp.com';
+export const API_URL = 'https://nv2mtil1t1.execute-api.eu-west-2.amazonaws.com/shop/products';
 // export const API_URL = 'http://localhost:5000';
